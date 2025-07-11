6a8284c7c9bd6292c5ec0c67e64dc69c2a160fcb
maksmaks21 <maksroks777@gmail.com>
2025-07-12 01:13:33 +0300

    settings

--- game/screens/settings.py
+++ game/screens/settings.py
@@ -2,6 +2,5 @@ from kivy.uix.screenmanager import Screen
 
 class SettingsScreen(Screen):
     def go_back(self):
-        # Повертаємось до головного меню з анімацією справа наліво
         self.manager.transition.direction = 'right'
         self.manager.current = 'main_menu'
